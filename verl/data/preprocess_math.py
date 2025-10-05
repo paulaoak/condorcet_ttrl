a4383108c51ffa5f2d1894ede53cf6ad1eda0d5a
paulaoak <pc2222@ic.ac.uk>
2025-10-05 12:43:36 +0100

    dont need difficulty for train split

--- verl/data/preprocess_math.py
+++ verl/data/preprocess_math.py
@@ -11,6 +11,35 @@ def make_map_fn(split, source=None):
                 data_source = source
             question = example.pop("prompt")
             solution = example.pop("answer")
+            
+
+            data = {
+                "data_source": data_source,
+                "prompt": [
+                    {
+                        "role": "user",
+                        "content": question,
+                    }
+                ],
+                "ability": "math",
+                "reward_model": {"style": "rule", "ground_truth": solution},
+                "extra_info": {
+                    "split": split,
+                    "index": f"{data_source}-{idx}",
+                },
+            }
+            return data
+
+        return process_fn
+
+def make_map_fn_difficulty(split, source=None):
+        def process_fn(example, idx):
+            if source is None:
+                data_source = example.pop("source")
+            else:
+                data_source = source
+            question = example.pop("prompt")
+            solution = example.pop("answer")
             difficulty_level = example.pop("difficulty")
             
 
@@ -42,7 +71,7 @@ if __name__ == '__main__':
     test_dataset = datasets.load_dataset("json", data_files=os.path.join(data_source, 'test.json'), split='train')
 
     train_dataset = train_dataset.map(function=make_map_fn("train", data_source), with_indices=True)
-    test_dataset = test_dataset.map(function=make_map_fn("test", data_source), with_indices=True)
+    test_dataset = test_dataset.map(function=make_map_fn_difficulty("test", data_source), with_indices=True)
 
     train_dataset.to_parquet(os.path.join(data_source, 'train.parquet'))
     test_dataset.to_parquet(os.path.join(data_source, 'test.parquet'))
